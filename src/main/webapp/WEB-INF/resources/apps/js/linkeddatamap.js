87c9c3829fecd80bcbc0adc0284aca93290871dc
reinvantveer <rein.van.t.veer@geodan.nl>
2016-06-21 15:43:41 +0200

    'brt' backMap option (untested), whitespace, indentation

--- src/main/webapp/WEB-INF/resources/apps/js/linkeddatamap.js
+++ src/main/webapp/WEB-INF/resources/apps/js/linkeddatamap.js
@@ -1,7 +1,7 @@
 /*
  * NAME     linkeddatamap.js
- * VERSION  1.6.2
- * DATE     2016-03-16
+ * VERSION  1.8.0
+ * DATE     2016-06-15
  *
  * Copyright 2012-2016
  *
@@ -43,216 +43,216 @@ var circleQuotient = 0.7;
 
 var numberRegexp = /^[-+]?([0-9]*\.[0-9]+|[0-9]+)([eE][-+]?[0-9]+)?/;
 
- /*
+/*
  * Parse WKT and return GeoJSON.
  *
  * @param {string} _ A WKT geometry
  * @return {?Object} A GeoJSON geometry object
  */
 function parse(_) {
-    var parts = _.split(";"),
-        _ = parts.pop(),
-        srid = (parts.shift() || "").split("=").pop();
+	var parts = _.split(";"),
+		_ = parts.pop(),
+		srid = (parts.shift() || "").split("=").pop();
 
-    var i = 0;
+	var i = 0;
 
-    function $(re) {
-        var match = _.substring(i).match(re);
-        if (!match) return null;
-        else {
-            i += match[0].length;
-            return match[0];
-        }
-    }
+	function $(re) {
+		var match = _.substring(i).match(re);
+		if (!match) return null;
+		else {
+			i += match[0].length;
+			return match[0];
+		}
+	}
 
-    function crs(obj) {
-        if (obj && srid.match(/\d+/)) {
-            obj.crs = {
-                type: 'name',
-                properties: {
-                    name: 'urn:ogc:def:crs:EPSG::' + srid
-                }
-            };
-        }
+	function crs(obj) {
+		if (obj && srid.match(/\d+/)) {
+			obj.crs = {
+				type: 'name',
+				properties: {
+					name: 'urn:ogc:def:crs:EPSG::' + srid
+				}
+			};
+		}
 
-        return obj;
-    }
+		return obj;
+	}
 
-    function white() { $(/^\s*/); }
+	function white() { $(/^\s*/); }
 
-    function multicoords() {
-        white();
-        var depth = 0, rings = [], stack = [rings],
-            pointer = rings, elem;
+	function multicoords() {
+		white();
+		var depth = 0, rings = [], stack = [rings],
+			pointer = rings, elem;
 
-        while (elem =
-            $(/^(\()/) ||
-            $(/^(\))/) ||
-            $(/^(\,)/) ||
-            $(numberRegexp)) {
-            if (elem == '(') {
-                stack.push(pointer);
-                pointer = [];
-                stack[stack.length - 1].push(pointer);
-                depth++;
-            } else if (elem == ')') {
-                pointer = stack.pop();
-                // the stack was empty, input was malformed
-                if (!pointer) return;
-                depth--;
-                if (depth === 0) break;
-            } else if (elem === ',') {
-                pointer = [];
-                stack[stack.length - 1].push(pointer);
-            } else if (!isNaN(parseFloat(elem))) {
-                pointer.push(parseFloat(elem));
-            } else {
-                return null;
-            }
-            white();
-        }
+		while (elem =
+			$(/^(\()/) ||
+			$(/^(\))/) ||
+			$(/^(\,)/) ||
+			$(numberRegexp)) {
+			if (elem == '(') {
+				stack.push(pointer);
+				pointer = [];
+				stack[stack.length - 1].push(pointer);
+				depth++;
+			} else if (elem == ')') {
+				pointer = stack.pop();
+				// the stack was empty, input was malformed
+				if (!pointer) return;
+				depth--;
+				if (depth === 0) break;
+			} else if (elem === ',') {
+				pointer = [];
+				stack[stack.length - 1].push(pointer);
+			} else if (!isNaN(parseFloat(elem))) {
+				pointer.push(parseFloat(elem));
+			} else {
+				return null;
+			}
+			white();
+		}
 
-        if (depth !== 0) return null;
-        return rings;
-    }
+		if (depth !== 0) return null;
+		return rings;
+	}
 
-    function coords() {
-        var list = [], item, pt;
-        while (pt =
-            $(numberRegexp) ||
-            $(/^(\,)/)) {
-            if (pt == ',') {
-                list.push(item);
-                item = [];
-            } else {
-                if (!item) item = [];
-                item.push(parseFloat(pt));
-            }
-            white();
-        }
-        if (item) list.push(item);
-        return list.length ? list : null;
-    }
+	function coords() {
+		var list = [], item, pt;
+		while (pt =
+			$(numberRegexp) ||
+			$(/^(\,)/)) {
+			if (pt == ',') {
+				list.push(item);
+				item = [];
+			} else {
+				if (!item) item = [];
+				item.push(parseFloat(pt));
+			}
+			white();
+		}
+		if (item) list.push(item);
+		return list.length ? list : null;
+	}
 
-    function point() {
-        if (!$(/^(point)/i)) return null;
-        white();
-        if (!$(/^(\()/)) return null;
-        var c = coords();
-        if (!c) return null;
-        white();
-        if (!$(/^(\))/)) return null;
-        return {
-            type: 'Point',
-            coordinates: c[0]
-        };
-    }
+	function point() {
+		if (!$(/^(point)/i)) return null;
+		white();
+		if (!$(/^(\()/)) return null;
+		var c = coords();
+		if (!c) return null;
+		white();
+		if (!$(/^(\))/)) return null;
+		return {
+			type: 'Point',
+			coordinates: c[0]
+		};
+	}
 
 	// Added: unsupported in WKT, but pretty handy for leaflet!
 	// TODO: Hier nog features bij stoppen, en dan nog call-back functie implementeren!
 	function circle() {
-        if (!$(/^(circle)/i)) return null;
-        white();
-        if (!$(/^(\()/)) return null;
-        var c = coords();
-        if (!c) return null;
-        white();
-        if (!$(/^(\))/)) return null;
-        return {
-            type: 'Point',
+		if (!$(/^(circle)/i)) return null;
+		white();
+		if (!$(/^(\()/)) return null;
+		var c = coords();
+		if (!c) return null;
+		white();
+		if (!$(/^(\))/)) return null;
+		return {
+			type: 'Point',
 			radius: circleQuotient*parseFloat(c[1]),
-            coordinates: c[0]
-        };
+			coordinates: c[0]
+		};
 	}
-	
-    function multipoint() {
-        if (!$(/^(multipoint)/i)) return null;
-        white();
-        var c = multicoords();
-        if (!c) return null;
-        white();
-        return {
-            type: 'MultiPoint',
-            coordinates: c
-        };
-    }
 
-    function multilinestring() {
-        if (!$(/^(multilinestring)/i)) return null;
-        white();
-        var c = multicoords();
-        if (!c) return null;
-        white();
-        return {
-            type: 'MultiLineString',
-            coordinates: c
-        };
-    }
+	function multipoint() {
+		if (!$(/^(multipoint)/i)) return null;
+		white();
+		var c = multicoords();
+		if (!c) return null;
+		white();
+		return {
+			type: 'MultiPoint',
+			coordinates: c
+		};
+	}
 
-    function linestring() {
-        if (!$(/^(linestring)/i)) return null;
-        white();
-        if (!$(/^(\()/)) return null;
-        var c = coords();
-        if (!c) return null;
-        if (!$(/^(\))/)) return null;
-        return {
-            type: 'LineString',
-            coordinates: c
-        };
-    }
+	function multilinestring() {
+		if (!$(/^(multilinestring)/i)) return null;
+		white();
+		var c = multicoords();
+		if (!c) return null;
+		white();
+		return {
+			type: 'MultiLineString',
+			coordinates: c
+		};
+	}
 
-    function polygon() {
-        if (!$(/^(polygon)/i)) return null;
-        white();
-        return {
-            type: 'Polygon',
-            coordinates: multicoords()
-        };
-    }
+	function linestring() {
+		if (!$(/^(linestring)/i)) return null;
+		white();
+		if (!$(/^(\()/)) return null;
+		var c = coords();
+		if (!c) return null;
+		if (!$(/^(\))/)) return null;
+		return {
+			type: 'LineString',
+			coordinates: c
+		};
+	}
 
-    function multipolygon() {
-        if (!$(/^(multipolygon)/i)) return null;
-        white();
-        return {
-            type: 'MultiPolygon',
-            coordinates: multicoords()
-        };
-    }
+	function polygon() {
+		if (!$(/^(polygon)/i)) return null;
+		white();
+		return {
+			type: 'Polygon',
+			coordinates: multicoords()
+		};
+	}
 
-    function geometrycollection() {
-        var geometries = [], geometry;
+	function multipolygon() {
+		if (!$(/^(multipolygon)/i)) return null;
+		white();
+		return {
+			type: 'MultiPolygon',
+			coordinates: multicoords()
+		};
+	}
 
-        if (!$(/^(geometrycollection)/i)) return null;
-        white();
+	function geometrycollection() {
+		var geometries = [], geometry;
 
-        if (!$(/^(\()/)) return null;
-        while (geometry = root()) {
-            geometries.push(geometry);
-            white();
-            $(/^(\,)/);
-            white();
-        }
-        if (!$(/^(\))/)) return null;
+		if (!$(/^(geometrycollection)/i)) return null;
+		white();
 
-        return {
-            type: 'GeometryCollection',
-            geometries: geometries
-        };
-    }
+		if (!$(/^(\()/)) return null;
+		while (geometry = root()) {
+			geometries.push(geometry);
+			white();
+			$(/^(\,)/);
+			white();
+		}
+		if (!$(/^(\))/)) return null;
 
-    function root() {
-        return point() ||
-            circle() ||
-            linestring() ||
-            polygon() ||
-            multipoint() ||
-            multilinestring() ||
-            multipolygon() ||
-            geometrycollection();
-    }
+		return {
+			type: 'GeometryCollection',
+			geometries: geometries
+		};
+	}
 
-    return crs(root());
+	function root() {
+		return point() ||
+			circle() ||
+			linestring() ||
+			polygon() ||
+			multipoint() ||
+			multilinestring() ||
+			multipolygon() ||
+			geometrycollection();
+	}
+
+	return crs(root());
 }
 
 function style(feature) {
@@ -264,8 +264,8 @@ function style(feature) {
 }
 
 function highlightFeature(e) {
-    var layer = e.target;
-	
+	var layer = e.target;
+
 	map.dragging.disable();
 	//Not for markers!
 	if (!(layer instanceof L.Marker)) {
@@ -280,7 +280,7 @@ function highlightFeature(e) {
 }
 
 function resetHighlight(e) {
-    var layer = e.target;
+	var layer = e.target;
 
 	map.dragging.enable();
 	//Not for markers!
@@ -320,7 +320,7 @@ function setChangeColors() {
 
 function circleMove(e) {
 	movedItem.setLatLng(e.latlng);
-	if (movedItem.label) {	
+	if (movedItem.label) {
 		movedItem.label.setLatLng(movedItem._latlng);
 	}
 	setChangeColors();
@@ -337,11 +337,11 @@ function circleMoveEnd(e) {
 }
 
 function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
+	layer.on({
+		mouseover: highlightFeature,
+		mouseout: resetHighlight,
 		mousedown: circleMoveStart
-    });
+	});
 }
 
 function pointToLayer(feature, latlng) {
@@ -373,7 +373,7 @@ function addWKT(uri, wkt, text, url, styleclass)
 		html+=text;
 	}
 	lastPolygon = L.geoJson(wktObject,{style: style,onEachFeature: onEachFeature,pointToLayer: pointToLayer}).addTo(map)
-						.bindPopup(html);
+		.bindPopup(html);
 	//TESTTESTTEST
 	lastPolygon.uri = uri;
 	//TESTTESTTEST
@@ -384,7 +384,7 @@ function addWKT(uri, wkt, text, url, styleclass)
 		var layer = lastPolygon.getLayers()[0];
 		var pos = map.latLngToLayerPoint(layer._latlng);
 		var labelPoint = map.layerPointToContainerPoint(pos);
-		
+
 		var tsvg = document.createElementNS("http://www.w3.org/2000/svg","text");
 		tsvg.setAttribute("x",labelPoint.x);
 		tsvg.setAttribute("y",labelPoint.y);
@@ -392,7 +392,7 @@ function addWKT(uri, wkt, text, url, styleclass)
 		tsvg.appendChild(thtm);
 		layer._path.parentNode.appendChild(tsvg);
 	}
-	
+
 	if (lastPolygon.bindLabel!=undefined) {
 		lastPolygon.bindLabel(text,{noHide:true, offset: [0,0]});
 		lastPolygon.showLabel();
@@ -407,7 +407,7 @@ function addEdge(subject,predicate,object) {
 
 	var sSub = listOfGeoObjects.filter(function(obj) {return obj.uri == subject;})[0];
 	var sObj = listOfGeoObjects.filter(function(obj) {return obj.uri == object;})[0];
-	
+
 	if (sSub!=undefined && sObj!=undefined) {
 		latlngs.push(sSub.getLayers()[0].getLatLng());
 		latlngs.push(sObj.getLayers()[0].getLatLng());
@@ -434,7 +434,7 @@ function updateMap() {
 	}
 	if (listOfMarkers.length>0) {
 		$.post(containerURL, {container: containerURL,content: updateTTL}, function(data) {alert(data.response)},'json');
-		
+
 		listOfMarkers = [];
 	}
 	mapChanged = false;
@@ -444,7 +444,7 @@ function addPoint(latCor, longCor, text, url)
 {
 	//Every location is a marker-object.
 	var location = L.marker();
-	
+
 	//Eerst stellen we de locatie in waarop de marker moet worden weergegeven, waarbij we hier kiezen voor simpele Latitude/Longitude coördinaten die ook gebruikt worden door GPS.
 	//location.setLatLng([latCor, longCor]);
 	//Daarna geven we aan welke informatie moet worden weergegeven wanneer de gebruiker op de locatie klikt.
@@ -455,7 +455,7 @@ function addPoint(latCor, longCor, text, url)
 		html+=text;
 	}
 	location.bindPopup(html).openPopup();
-	
+
 	//Tot slot voegen we de locatie toe aan de te tonen locaties.
 	listOfLocations.push(location);
 }
@@ -467,14 +467,14 @@ function printMap() {
 	var img = document.getElementsByClassName("leaflet-image-layer")[0]; //Dit mag beter: er zijn mogelijk meerdere img met deze classname
 
 	//Size of the container should be the size of the image (don't know how to do this :-(
-	
+
 	//Zoom should be 1:1
 	//Styleclass declarations can not be exported: copy styleclass values to style attributes
 	for(i = 0; i < listOfGeoObjects.length; ++i) {
 		var pathStyle = getComputedStyle(listOfGeoObjects[i].getLayers()[0]._path);
 		listOfGeoObjects[i].setStyle({color: pathStyle.getPropertyValue("stroke"), fillColor: pathStyle.getPropertyValue("fill"), fillOpacity: pathStyle.getPropertyValue("fill-opacity")});
 	}
-	
+
 	var svg = document.getElementsByTagName("svg")[0]; //Dit mag beter: er zijn mogelijk meerdere svg's!
 
 	var svg_xml = (new XMLSerializer).serializeToString(svg.parentNode);
@@ -494,20 +494,18 @@ function mapClicked(e) {
 	form.submit();
 }
 
-function initMap(docroot, latCor, longCor, backMap, imageMapURL, contURL, left, top, width, height)
-{
+function initMap(docroot, latCor, longCor, backMap, imageMapURL, contURL, left, top, width, height) {
 	// Pad naar de icons goedmaken
 	L.Icon.Default.imagePath = docroot + '/images';
 
-	if (backMap=='image') {
-	
+	if (backMap === 'image') {
 		// create the slippy map
 		map = L.map('map', {
-		  minZoom: 1,
-		  maxZoom: 4,
-		  center: [0, 0],
-		  zoom: 2,
-		  crs: L.CRS.Simple
+			minZoom: 1,
+			maxZoom: 4,
+			center: [0, 0],
+			zoom: 2,
+			crs: L.CRS.Simple
 		});
 
 		// calculate the edges of the image, in coordinate space
@@ -515,38 +513,53 @@ function initMap(docroot, latCor, longCor, backMap, imageMapURL, contURL, left, 
 		var northEast = map.unproject([width+left, top], map.getMaxZoom()-1);
 		var bounds = new L.LatLngBounds(southWest, northEast);
 
-		// add the image overlay, 
+		// add the image overlay,
 		// so that it covers the entire map
 		osm = new L.imageOverlay(imageMapURL, bounds);
 		map.addLayer(osm);
 
 		// tell leaflet that the map is exactly as big as the image
 		map.setMaxBounds(bounds);
-		
+
 		// Add print-button
 		L.easyButton( '<span class="print"><b>P</b></span>', printMap).addTo(map);
-		
+
 		// Add save-button
 		if (contURL!='') {
 			containerURL = contURL;
 			L.easyButton( '<span class="save"><b>S</b></span>', updateMap).addTo(map);
 		}
-	}
-	else {
-	
-		if (backMap=='brt') {
+	} else {
+		//RD Projectie
+		var RD = L.CRS.proj4js('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', new L.Transformation(1, 285401.920, -1, 903401.920));
+		RD.scale = function(zoom) {
+			return 1 / res[zoom];
+		};
+
+		if (backMap === 'brt') {
 			//Use BRT tiles
-			//RD Projectie
-			var RD = L.CRS.proj4js('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', new L.Transformation(1, 285401.920, -1, 903401.920));
-			RD.scale = function(zoom) {
-				return 1 / res[zoom];
-			};
-			map = L.map('map',{crs: RD});
-			osm = new L.TileLayer('http://geodata.nationaalgeoregister.nl/tms/1.0.0/brtachtergrondkaart/{z}/{x}/{y}.png', {minZoom: 1, maxZoom: 13, tms: true, continuousWorld: true});
+			map = L.map('map', { crs: RD });
+			osm = new L.TileLayer(
+				'http://geodata.nationaalgeoregister.nl/tms/1.0.0/brtachtergrondkaart/{z}/{x}/{y}.png', {
+					minZoom: 1,
+					maxZoom: 13,
+					tms: true,
+					continuousWorld: true
+				}
+			);
+		} else if (backMap === 'brk') {
+			//Use BRK tiles
+			map = L.map('map', { crs: RD });
+			osm = new L.TileLayer.wms(
+				'https://geodata.nationaalgeoregister.nl/wms/kadastralekaartv2', {
+					layers: 'kadastralekaartv2',
+					format: 'image/png'
+				}
+			);
 		} else {
 			//Use OpenStreetMap tiles
 			map = L.map('map');
-			osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {minZoom: 1, maxZoom: 18});
+			osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { minZoom: 1, maxZoom: 18 });
 		}
 		//We initialiseren de kaart met een set van coördinaten waarbij we hier kiezen voor simpele Latitude/Longitude coördinaten die ook gebruikt worden door GPS. De derde parameter is het standaard zoom-niveau van de kaart. Bij zoomen geldt: hoe hoger, hoe dichter bij.
 		map.setView(new L.LatLng(latCor, longCor), 5);
@@ -560,22 +573,21 @@ function initMap(docroot, latCor, longCor, backMap, imageMapURL, contURL, left, 
 	map.invalidateSize();
 }
 
-function showLocations(doZoom, appearance)
-{
+function showLocations(doZoom, appearance) {
 	d3.select(map.getPanes().overlayPane).selectAll("g").append('marker')
-	.attr('id'          , 'ArrowHead')
-	.attr('viewBox'     , '0 -5 10 10')
-	.attr('refX'        , 10)
-	.attr('refY'        , 0)
-	.attr('markerWidth' , 6)
-	.attr('markerHeight', 6)
-	.attr('orient'      , 'auto')
-	.attr('stroke'		, '#909090')
-	.attr('fill'		, 'none')
-	.attr('stroke-width', '2px')
-  .append('path')
-	.attr('d', 'M0,-5L10,0L0,5');
-	
+		.attr('id'          , 'ArrowHead')
+		.attr('viewBox'     , '0 -5 10 10')
+		.attr('refX'        , 10)
+		.attr('refY'        , 0)
+		.attr('markerWidth' , 6)
+		.attr('markerHeight', 6)
+		.attr('orient'      , 'auto')
+		.attr('stroke'		, '#909090')
+		.attr('fill'		, 'none')
+		.attr('stroke-width', '2px')
+		.append('path')
+		.attr('d', 'M0,-5L10,0L0,5');
+
 	//Add all locations to the map
 	for(i = 0; i < listOfLocations.length; ++i)
 		listOfLocations[i].addTo(map);
@@ -592,5 +604,4 @@ function showLocations(doZoom, appearance)
 		map.on('click',mapClicked);
 		document.getElementById('map').style.cursor = 'crosshair';
 	}
-	
 }
